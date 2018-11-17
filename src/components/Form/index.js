bebe9f2f3cf7c0164772ef466eb176040d879d47
jrbreckenitch <jamesbreckenitch@Jamess-MacBook-Pro.local>
2018-11-17 15:45:33 -0600

    style update

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -17,22 +17,22 @@ class Form extends Component {
         <h1>Hello World!</h1>
         <div id="photoBox">
           <div className="row">
-            <div id="box1" className="boxes"></div>
-            <div id="box2" className="boxes"></div>
-            <div id="box3" className="boxes"></div>
-            <div id="box4" className="boxes"></div>
+            <div id="box1" className="btn boxes"></div>
+            <div id="box2" className="btn boxes"></div>
+            <div id="box3" className="btn boxes"></div>
+            <div id="box4" className="btn boxes"></div>
           </div>
           <div className="row">
-            <div id="box5" className="boxes"></div>
-            <div id="box6" className="boxes"></div>
-            <div id="box7" className="boxes"></div>
-            <div id="box8" className="boxes"></div>
+            <div id="box5" className="btn boxes"></div>
+            <div id="box6" className="btn boxes"></div>
+            <div id="box7" className="btn boxes"></div>
+            <div id="box8" className="btn boxes"></div>
           </div>
           <div className="row">
-            <div id="box9" className="boxes"></div>
-            <div id="box10" className="boxes"></div>
-            <div id="box11" className="boxes"></div>
-            <div id="box12" className="boxes"></div>
+            <div id="box9" className="btn boxes"></div>
+            <div id="box10" className="btn boxes"></div>
+            <div id="box11" className="btn boxes"></div>
+            <div id="box12" className="btn boxes"></div>
           </div>
         </div>
       </div>
